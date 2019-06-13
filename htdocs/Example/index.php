f4879c3e9d5c014545eaaf77fa38e84979a925a3
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-06-13 11:36:40 +0800

    htdocs and MySQL new added ThesisLibrary.sql

---
+++ htdocs/Example/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+* {box-sizing: border-box;}
+ul {list-style-type: none;}
+body {font-family: Verdana, sans-serif;}
+
+.month {
+  padding: 70px 25px;
+  width: 100%;
+  background: #1abc9c;
+  text-align: center;
+}
+
+.month ul {
+  margin: 0;
+  padding: 0;
+}
+
+.month ul li {
+  color: white;
+  font-size: 20px;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+}
+
+.month .prev {
+  float: left;
+  padding-top: 10px;
+}
+
+.month .next {
+  float: right;
+  padding-top: 10px;
+}
+
+.weekdays {
+  margin: 0;
+  padding: 10px 0;
+  background-color: #ddd;
+}
+
+.weekdays li {
+  display: inline-block;
+  width: 13.6%;
+  color: #666;
+  text-align: center;
+}
+
+.days {
+  padding: 10px 0;
+  background: #eee;
+  margin: 0;
+}
+
+.days li {
+  list-style-type: none;
+  display: inline-block;
+  width: 13.6%;
+  text-align: center;
+  margin-bottom: 5px;
+  font-size:12px;
+  color: #777;
+}
+
+.days li .active {
+  padding: 5px;
+  background: #1abc9c;
+  color: white !important
+}
+
+/* Add media queries for smaller screens */
+@media screen and (max-width:720px) {
+  .weekdays li, .days li {width: 13.1%;}
+}
+
+@media screen and (max-width: 420px) {
+  .weekdays li, .days li {width: 12.5%;}
+  .days li .active {padding: 2px;}
+}
+
+@media screen and (max-width: 290px) {
+  .weekdays li, .days li {width: 12.2%;}
+}
+</style>
+</head>
+<body>
+
+<h1>CSS Calendar</h1>
+
+<div class="month">      
+  	<ul>
+	    <a href=""><li class="prev">&#10094; </li></a>
+	    <li class="next">&#10095;</li>
+	    <li>
+	      	August<br>
+	      	<span style="font-size:18px">2017</span>
+	    	</li>
+  	</ul>
+</div>
+
+<ul class="weekdays">
+  
+</ul>
+
+<ul class="days">  
+  	<?php $ctr = 1;?>
+  	<?php while ($ctr <= 31): ?>
+	   	<li class="<?=$cssClass?>"> 
+	   		<?php if($ctr==25){ ?> <span class="active">  <?php echo $ctr; ?> </span> <?php } else { ?>  <?php echo $ctr; ?> <?php } ?></li>
+	   	<?php $ctr = $ctr + 1;?>
+	<?php endwhile; ?>
+</ul>
+
+</body>
+</html>
+
+
